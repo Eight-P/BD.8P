7382cdee2eea8b6455b6a23b4e3850428c1e326c
Eight-P <29176216+Eight-P@users.noreply.github.com>
2024-08-04 12:24:08 +0200

    compile script v3

--- Themes/scripts/compileDevTheme.js
+++ Themes/scripts/compileDevTheme.js
@@ -1,5 +1,5 @@
-// * compileDevTheme.js v2.1
-// watch and compile dev file to mod folder
+// * compileDevTheme.js v3
+// watch and compile dev file to mod folders
 
 const Fs = require('fs');
 const Path = require('path');
@@ -8,15 +8,15 @@ const Sass = require('sass');
 
 const ThemeFolder = process.cwd();
 const ThemeName = Path.basename(ThemeFolder);
-const DevFile = checkPath(Path.join(ThemeFolder, "dev", ThemeName + "-dev.theme.scss"));
-const DevFileOut = Path.join(getModFolder(), ThemeName + "-dev.theme.css");
+const InputFile = checkPath(Path.join(ThemeFolder, "dev", ThemeName + "-dev.theme.scss"));
+const OutputFiles = getOutputPaths();
 
 // for debug
 // console.log(Paths = {
 //   name: ThemeName,
 //   folder: ThemeFolder,
-//   dev: DevFile,
-//   devOut: DevFileOut
+//   devIn: InputFile,
+//   devOut: OutputFiles
 // });
 
 const SassOptions = {
@@ -27,33 +27,37 @@ const SassOptions = {
 
 Chokidar.watch(ThemeFolder)
   .on('ready', () => {
-    console.log(`Listening for changes in: \u001b[36m${ThemeFolder}\u001b[0m`);
-    console.log(`Compiling dev file to: \u001b[36m${DevFileOut}\u001b[0m \n`);
+    console.log(`Listening for changes in:\n\u001b[36m${ThemeFolder}\u001b[0m\nCompiling to:`);
+    for (let filePath of OutputFiles) {
+      console.log(`\u001b[36m${filePath}\u001b[0m \n`);
+    }
     compileToCss()
   })
-  .on('change', (trigger) => {
-    compileToCss(trigger)
-});
+  .on('change', (trigger) => { compileToCss(trigger) }
+);
 
 function compileToCss(trigger) {
   try {
-    let output = Sass.compile(DevFile, SassOptions);
-    Fs.writeFileSync(DevFileOut, output.css);
+    let output = Sass.compile(InputFile, SassOptions);
 
+    for (let filePath of OutputFiles) {
+      Fs.writeFile(filePath, output.css, (err) => {
+        if (err) { throw err }
+      })
+    }
     if (trigger) {
       let fileName = trigger.split(Path.sep).at(-1);
-      console.log(`${getTimestamp()} recompiled for: \u001b[34m${fileName}\u001b[0m`);
+      console.log(`${getTimestamp()} Recompiled \u001b[34m${fileName}\u001b[0m`);
     }
-  } 
-  catch (err) {
-    console.error(err)
+  }
+  catch (err) { 
+    console.error(err) 
   }
 }
 
-function getModFolder() {
-
-  const arg = process.argv[2]
-  const modArg = arg ? arg.toLowerCase() : "bd" ;
+function getOutputPaths() {
+  let pathsList = [];
+  let argList = process.argv.slice(2);
 
   const modNames = {
     bd: 'betterdiscord',
@@ -62,19 +66,30 @@ function getModFolder() {
     vencord: 'vencord'
   }
 
-  const mod = modNames[modArg];
+  function getModFolder(mod) {
+    const modFolders = { // I think these are correct, but not sure
+      win32: `${process.env.APPDATA}\\${mod}\\themes`,
+      darwin: `${process.env.HOME}/Library/Application Support/${mod}/themes`,
+      linux: `${process.env.HOME}/.config/${mod}/themes`
+    }
 
-  if (!mod) quitWithError(new Error(), "Invalid mod", arg);
-
-  const modFolders = { // I think these are correct, but not sure
-    win32: `${process.env.APPDATA}\\${mod}\\themes`,
-    darwin: `${process.env.HOME}/Library/Application Support/${mod}/themes`,
-    linux: `${process.env.HOME}/.config/${mod}/themes`
+    return checkPath(modFolders[process.platform])
   }
 
-  const folder = checkPath(modFolders[process.platform]);
-  
-  return folder
+  if (argList[0]) {
+    for (let arg of argList) {
+      let modname = modNames[arg.toLowerCase()];
+      if (!modname) quitWithError(new Error(), "Invalid mod", arg);
+      let filePath = Path.join(getModFolder(modname), ThemeName + "-dev.theme.css");
+      pathsList.push(filePath);
+    }
+  }
+  else {
+    let filePath = Path.join(getModFolder("betterdiscord"), ThemeName + "-dev.theme.css");
+    pathsList.push(filePath);
+  }
+
+  return pathsList ;
 }
 
 function checkPath(path) {
@@ -95,11 +110,10 @@ function getTimestamp() {
   let ss = date.getSeconds();
   let seconds = ((ss < 10) ? '0' + ss : ss);
 
-  return `[\u001b[32m${hour}:${minutes}:${seconds}\u001b[0m]`
+  return `\u001b[32m${hour}:${minutes}:${seconds}\u001b[0m`
 }
 
 function quitWithError(err, msg, details) {
-
   err.message = `\n\u001b[31m${msg}:\u001b[0m \u001b[4m${details}\u001b[0m \n` ;
   console.error(err);
   console.error("\n");
