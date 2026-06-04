1535556f2e02a9c9544100c5cfd297b26ac591c1
Eight_P <29176216+Eight-P@users.noreply.github.com>
2026-06-04 21:46:17 +0200

    devScript v5 *buildAction

--- Themes/scripts/compileDevTheme.js
+++ Themes/scripts/compileDevTheme.js
@@ -1,4 +1,4 @@
-// * compileDevTheme.js v4.3
+// * compileDevTheme.js v5
 // watch and compile dev files to mod folders
 
 const Fs = require('fs');
@@ -6,36 +6,48 @@ const Path = require('path');
 const Chokidar = require('chokidar');
 const Sass = require('sass-embedded');
 
-const Setup = getSetup();
-const DevFiles = getDevFiles();
-const OutputPaths = getOutputPaths();
-
-const Compiler = Sass.initCompiler();
-
-const SassOptions = {
-  style: 'expanded',
-  sourceMap: false,
-  sourceMapIncludeSources: false,
-  silenceDeprecations: ["import", "if-function"]
+let Config = {
+  watchPaths: [process.cwd()],
+  devFiles: new Set(),
+  outputPaths: new Set(),
+  Sass: {
+    style: 'expanded',
+    sourceMap: false,
+    sourceMapIncludeSources: false,
+    silenceDeprecations: ['import', 'if-function'],
+    loadPaths: []
+  }
 };
 
-Chokidar.watch(Setup.watchFolder, {
-  ignored: (path, stats) => stats?.isFile() && !path.endsWith('.scss')
+setupArgs(); setupDevFiles();
+const Compiler = Sass.initCompiler();
+
+
+Chokidar.watch(Config.watchPaths, {
+  ignored: (path, stats) => {
+    if (path.includes('node_modules')) return true ;
+    if (stats?.isFile() && !path.endsWith('.scss')) return true ;
+    return false ;
+  }
 })
   .on('ready', () => {
-    console.log(`Listening for changes in:\n \u001b[36m${Setup.watchFolder}\u001b[0m \nInput:`);
-    for (let devFile of DevFiles) {
-      console.log(` \u001b[36m${devFile}\u001b[0m`);
+    console.log(`Listening for changes in:`);
+    for (const watchPath of Config.watchPaths) {
+      console.log(` ${Cyan(watchPath)}`);
+    }
+    console.log("Input:");
+    for (const devFile of Config.devFiles) {
+      console.log(` ${Cyan(devFile)}`);
     }
     console.log("Output:");
-    for (let filePath of OutputPaths) {
-      console.log(` \u001b[36m${filePath}\u001b[0m`);
+    for (const filePath of Config.outputPaths) {
+      console.log(` ${Cyan(filePath)}`);
     }
     console.log("\n");
     upateDevFiles();
   })
   .on('change', (trigger) => {
-    upateDevFiles(Path.parse(trigger).name);
+    upateDevFiles(trigger);
   })
   .on('error', (err) => { console.error(`Chokidar error:\n${err}`) })
 ;
@@ -43,132 +55,131 @@ Chokidar.watch(Setup.watchFolder, {
 function upateDevFiles(trigger) {
   let hadError = false ;
 
-  for (let devFile of DevFiles) {
+  for (const devFile of Config.devFiles) {
     let output ;
     try {
-      output = Compiler.compile(devFile, SassOptions);
+      output = Compiler.compile(devFile, Config.Sass);
     }
     catch (err) {
-      if (DevFiles.length > 1) {
-        console.error(`\n\u001b[31mError using Dev-file:\u001b[0m \u001b[34m${Path.basename(devFile)}\u001b[0m\n`);
+      if (Config.devFiles.size > 1) {
+        console.error(`\n${Red("Error using Dev-file")}: ${Cyan(Path.basename(devFile))}\n`);
       }
       console.error(err);
       hadError = true ;
       continue ;
     }
 
-    let devFileName = Path.parse(devFile).name ;
+    const devFileName = Path.parse(devFile).name ;
 
-    for (let outputPath of OutputPaths) {
-      outputFile = Path.join(outputPath, devFileName + '.css');
+    for (let outputPath of Config.outputPaths) {
+      const outputFile = Path.join(outputPath, devFileName + '.css');
 
       // sync seems better for bd
       Fs.writeFileSync(outputFile, output.css, (err) => {
-        if (err) { throw err }
+        if (err) throw err ;
       });
     }
   }
   if (trigger && !hadError) {
-    console.log(`${getTimestamp()} Recompiled \u001b[34m${trigger}\u001b[0m`);
+    const triggerName = Path.parse(trigger).name ;
+    console.log(getTimestamp() + " " + Blue(triggerName));
   }
 }
 
-function getSetup() {
-  let config = {
-    watchFolder: "",
-    mods: []
+function setupArgs() {
+  const argsList = {
+    lp: (path) => {
+      const lp = checkPath(Path.resolve(path));
+      Config.Sass.loadPaths.push(lp);
+
+      if (!lp.startsWith(process.cwd())) {
+        Config.watchPaths.push(lp);
+      }
+    },
+    bd: () => {
+      Config.outputPaths.add(getModFolder('BetterDiscord'));
+    },
+    ven: () => {
+      Config.outputPaths.add(getModFolder('Vencord'));
+    },
+    vesk: () => {
+      Config.outputPaths.add(getModFolder('vesktop'));
+    },
+
+    loadpath: (path) => { argsList.lp(path) },
+    betterdiscord: () => { argsList.bd() },
+    vencord: () => { argsList.ven() },
+    vesktop: () => { argsList.vesk() }
   };
-  let argList = process.argv.slice(2);
-  let useInitDirArg = argList.indexOf("init_dir");
 
-  if (useInitDirArg != -1 ) {
-    config.watchFolder = process.env.INIT_CWD ;
-    argList.splice(useInitDirArg, 1);
-  }
-  else {
-    config.watchFolder = process.cwd();
-  }
-  config.mods = argList ;
+  let processArgs = new Set(); // no duplicates
 
-  return config ;
+  for (const arg of process.argv.slice(2)) {
+    processArgs.add(arg.toLowerCase());
+  }
+
+  for (const fullArg of processArgs) {
+    let splitArg = fullArg.split("=");
+    let argName = splitArg[0];
+
+    if (argsList[argName] && !splitArg[2]) {
+      argsList[argName](splitArg[1]);
+    }
+    else {
+      quitWithError(new Error(), "invalid argument", fullArg);
+    }
+  }
+
+  if (Config.outputPaths.size == 0) {
+    Config.outputPaths.add(getModFolder('BetterDiscord'));
+  }
 }
 
-function getDevFiles() {
-  let devFiles = [] ;
-  let devFolder = checkPath(Path.join(process.cwd(), 'dev'));
-  let dirEntries = Fs.readdirSync(devFolder, { withFileTypes:true });
+function setupDevFiles() {
+  const devFolder = checkPath(Path.join(process.cwd(), 'dev'));
+  const dirEntries = Fs.readdirSync(devFolder, { withFileTypes:true });
 
-  for (let entry of dirEntries) {
+  for (const entry of dirEntries) {
     if (entry.isFile() && entry.name.endsWith('.theme.scss')) {
-      devFiles.push(Path.join(entry.parentPath, entry.name));
+      Config.devFiles.add(Path.join(entry.parentPath, entry.name));
     }
   }
-  if (devFiles[0]) {
-    return devFiles ;
+  if (Config.devFiles.size < 1) {
+    quitWithError(new Error(), "Dev files need to end in .theme.scss. No dev file found in", devFolder);
   }
-  quitWithError(new Error(), "Dev files need to end in .theme.scss. No dev file found in", devFolder);
 }
 
-function getOutputPaths() {
-  let pathsList = [];
-
-  const modNames = {
-    bd: 'betterdiscord',
-    betterdiscord: 'betterdiscord',
-    ven: 'vencord',
-    vencord: 'vencord',
-    vesk: 'vesktop',
-    vesktop: 'vesktop'
+function getModFolder(mod) {
+  const modFolders = { // I think these are correct, but not sure
+    win32: `${process.env.APPDATA}\\${mod}\\themes`,
+    darwin: `${process.env.HOME}/Library/Application Support/${mod}/themes`,
+    linux: `${process.env.HOME}/.config/${mod}/themes`
   }
 
-  function getModFolder(mod) {
-    const modFolders = { // I think these are correct, but not sure
-      win32: `${process.env.APPDATA}\\${mod}\\themes`,
-      darwin: `${process.env.HOME}/Library/Application Support/${mod}/themes`,
-      linux: `${process.env.HOME}/.config/${mod}/themes`
-    }
-
-    return checkPath(modFolders[process.platform])
-  }
-
-  if (Setup.mods[0]) {
-    for (let mod of Setup.mods) {
-      let modName = modNames[mod.toLowerCase()];
-      if (!modName) quitWithError(new Error(), "Invalid argument for mod", mod);
-      pathsList.push(getModFolder(modName));
-    }
-  }
-  else {
-    pathsList.push(getModFolder("betterdiscord"));
-  }
-
-  return pathsList ;
+  return checkPath(modFolders[process.platform]);
 }
 
 function checkPath(path) {
   if (Fs.existsSync(path)) {
-    return path
+    return path ;
   } else {
-    quitWithError(new Error(), "Can not find path", path)
+    quitWithError(new Error(), "Can not find path", path);
   }
 }
 
 function getTimestamp() {
-  let date = new Date();
-
-  let hh = date.getHours();
-  let hour = ((hh < 10) ? '0' + hh : hh);
-  let mm = date.getMinutes();
-  let minutes = ((mm < 10) ? '0' + mm : mm);
-  let ss = date.getSeconds();
-  let seconds = ((ss < 10) ? '0' + ss : ss);
-
-  return `\u001b[32m${hour}:${minutes}:${seconds}\u001b[0m`
+  const now = new Date();
+  return Green(now.toLocaleTimeString('en-GB'));
 }
 
+const Cyan = (text) => `\u001b[36m${text}\u001b[0m` ;
+      Red = (text) => `\u001b[31m${text}\u001b[0m` ;
+      Green = (text) => `\u001b[32m${text}\u001b[0m` ;
+      Blue = (text) => `\u001b[34m${text}\u001b[0m` ;
+
 function quitWithError(err, msg, details) {
-  err.message = `\n\u001b[31m${msg}:\u001b[0m \u001b[4m${details}\u001b[0m \n` ;
+  err.message = `\n${Red(msg)}: ${details}\n` ;
   console.error(err);
   console.error("\n");
-  process.exit(1)
+  process.exit(1);
 }
